2fe40702eb643b7a2d02b368288d26e665b33784
Michael Wagner <mdwranger@gmail.com>
2023-01-17 08:47:11 -0500

    feat(wip): compile + watch assets

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
-import Alpine from "alpinejs";
+//import Alpine from "alpinejs";
 
-window.Alpine = Alpine;
+//window.Alpine = Alpine;
 
-Alpine.start();
+//Alpine.start();
+
+console.log("hello world!");
